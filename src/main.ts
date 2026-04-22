8788c0460bd4baf5b0e20ea29749d35c5c9d3b04
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2026-04-22 04:09:21 -0300

    refactor: Improve load the browser language and store on the localstorage.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-/// <reference types="@angular/localize" />
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
@@ -9,6 +7,16 @@ import localePt from '@angular/common/locales/pt';
 
 registerLocaleData(localePt);
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
-);
+const savedLang = localStorage.getItem('lang');
+const browserLang = navigator.language.startsWith('pt') ? 'pt' : 'en-US';
+const lang = savedLang || browserLang;
+
+const path = window.location.pathname;
+
+if (path === '/' || path === '') {
+  window.location.replace(`/${lang}`);
+} else {
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err),
+  );
+}
